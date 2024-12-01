9263ba96cfe681483b8b3893d6de2472d73df8aa
root <root@ip-172-31-53-156.us-west-2.compute.internal>
2024-12-01 05:25:57 +0000

    Added code to validate user input

--- pass_gen.sh
+++ pass_gen.sh
@@ -1,19 +1,35 @@
-#!/binbash
-# Author: Pratyusha
-# Introduction message
+#!/bin/bash
+# Author: Pratyusha 
+
+# Introducation message
 echo "Time to generate some passwords"
 sleep 1
 
-# Prompt the user for password length
-echo "Enter the length of the password:"
-read pass_leng
+# Prompting the user for password length and validate the input
+while true; do
+    echo "Enter the length of the password:"
+    read pass_leng
+    if [[ "$pass_leng" =~ ^[0-9]+$ ]] && [ "$pass_leng" -gt 0 ]; then
+        break
+    else
+        echo "Your input is invalid"
+        echo "Please enter valid input"
+    fi
+done
 
-# Prompt the user for the number of passwords
-echo "Enter how many times you want to generate the password:"
-read pass_num
+# Prompting the user for the number of passwords and validate the input
+while true; do
+    echo "Enter how many times you want to generate the password:"
+    read pass_num
+    if [[ "$pass_num" =~ ^[0-9]+$ ]] && [ "$pass_num" -gt 0 ]; then
+        break
+    else
+        echo "Your input is invalid"
+        echo "Please enter valid input"
+    fi
+done
 
 # Loop to generate the passwords
 for p in $(seq 1 $pass_num); do
-    # Generate random base64 string and trim it to the desired length
     openssl rand -base64 48 | cut -c1-$pass_leng
-done 
+done
