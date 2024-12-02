fa2fe86f8af4666e69d8c9d361151ba6de4c8bc8
tpratyushareddy <tpratyushareddy123@gmail.com>
2024-12-02 05:27:49 +0000

    Enable user to define partamenters via arguments

--- password-gen/pass_gen.sh
+++ password-gen/pass_gen.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Author: Pratyusha 
 
+set -e
+
 # Introducation message
 echo "Time to generate some passwords"
 sleep 1
 
-# Prompting the user for password length and validate the input
+<<Block_comment
+# Prompting the user for password length and validate the input 
 while true; do
     echo "Enter the length of the password:"
     read pass_leng
@@ -13,7 +16,8 @@ while true; do
         break
     else
         echo "Your input is invalid"
-        echo "Please enter valid input"
+        exit 1
+        #echo "Please enter valid input"
     fi
 done
 
@@ -25,11 +29,45 @@ while true; do
         break
     else
         echo "Your input is invalid"
-        echo "Please enter valid input"
+        exit 1
+        #echo "Please enter valid input"
     fi
 done
+Block_comment
+
+if [[ $# -ne 2 ]]; then
+     echo "Program requires 2 areguments. Either Missing or not specified" 
+     # Prompting the user for password length and validate the input
+     while true; do
+         echo "Enter the length of the password:"
+         read pass_leng
+         if [[ "$pass_leng" =~ ^[0-9]+$ ]] && [ "$pass_leng" -gt 0 ]; then
+             break
+         else
+             echo "Your input is invalid"
+             exit 1
+             #echo "Please enter valid input"
+         fi
+     done
+     # Prompting the user for the number of passwords and validate the input
+     while true; do
+         echo "Enter how many times you want to generate the password:"
+         read pass_num
+         if [[ "$pass_num" =~ ^[0-9]+$ ]] && [ "$pass_num" -gt 0 ]; then
+             break
+         else
+             echo "Your input is invalid"
+             exit 1
+             #echo "Please enter valid input"
+         fi
+      done
+else
+    pass_leng=$1
+    pass_num=$2
+fi
 
 # Loop to generate the passwords
 for p in $(seq 1 $pass_num); do
     openssl rand -base64 48 | cut -c1-$pass_leng
 done
+
